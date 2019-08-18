add45f5078bc4faf28968fb8202c8153adb751c5
unknown <raje1reddy@gmail.com>
2019-08-18 21:23:50 +0100

    calling method using onjects

---
+++ src/main/java/Employee.java
@@ -0,0 +1,13 @@
+class Employee {
+    private String name;
+    int age;      //object fields
+    Employee() {
+        age = 22; //assign value to age
+    }
+    public void setName(String val){
+        name = val; //assign value to name
+    }
+    public void printEmp(){
+        System.out.println("name " + name + "age" +  "" +age );
+    }
+}
